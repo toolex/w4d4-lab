0df6dc90a3d7c40c3bee0f6b9db2a8a32baf0bf8
Iain Tooley <iaintooley1@gmail.com>
2019-10-03 10:39:33 +0100

    added some stuff

--- student_controller.rb
+++ student_controller.rb
@@ -5,6 +5,22 @@ require( 'pry-byebug' )
 require_relative("./models/house_assignment")
 also_reload("./models/*")
 
-get "/" do
-  @
+get "/hogwarts" do
+  @students = Student.all
+  erb(:index)
+end
+
+get "/hogwarts/new" do
+  erb(:new)
+end
+
+get "/hogwarts/:id" do
+  @student = Student.find(params[:id])
+  erb(:show)
+end
+
+post "/hogwarts" do
+  @student = Student.new(params)
+  @student.save()
+  erb(:create)
 end
